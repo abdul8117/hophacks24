72315d1e0328ab1c79f9cdcbc8618da88c6de869
Abdul <abdulahad2004@yahoo.com>
2024-09-14 21:33:36 -0400

    Convert hard coded data to dynamic data

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ import Layout from '@/components/Layout';
 import NutrientChart from '@/components/NutrientChart';
 import UploadForm from '@/components/UploadForm';
 import MealTable from '@/components/MealTable';
-import { useState } from 'react';
+import HomePage from './homepage/page';
+import { useEffect, useState } from 'react';
+import API_URL from '@/utils/utils';
 
 export default function Home() {
   const [meals, setMeals] = useState([
@@ -13,36 +15,33 @@ export default function Home() {
     { meal: "Lunch: Chicken Salad", calories: 600, proteins: 45, fats: 30, carbs: 40 },
   ]);
 
+  let authenticatd = false;
+
+  // useEffect(() => {
+  //   fetch(`${API_URL}/validate-session`, {
+  //     method: "POST",
+  //     credentials: "include",
+  //   })
+  //     .then((res) => {
+  //       if (res.status === 200) {
+  //         authenticatd = true;
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
   return (
     <Layout>
-      <div className="flex flex-col md:flex-row min-h-screen justify-between">
-        {/* Left side with chart */}
-        <div className="w-full md:w-1/2 p-6">
-          <h2 className="text-2xl font-bold mb-2 mt-6 flex justify-center">Hey! Lets see how you are doing so far...</h2>
-          <NutrientChart />
-        </div>
+      {/* {authenticatd ? (
+        <HomePage />
+      ) : (
+        //<Auth /> // TODO
+        null
+      )} */}
 
-        {/* Right side with form */}
-        <div className="w-full md:w-1/2 p-6 flex flex-col items-center mt-5 gap-5 justify-start">
-          {/* Meals Table */}
-          <MealTable meals={meals} />
-
-          {/* Upload Form */}
-          <UploadForm />
-
-          {/* Key Takeaways */}
-          <div className="key-takeaways mt-6 p-6 rounded-lg w-full">
-            <h2 className="text-2xl font-bold mb-2">Key Takeaways</h2>
-            <p>Gemini suggests: Increase protein intake for better muscle recovery. DO IT BRO. </p>
-            <p>Consider adding more healthy fats for balanced nutrition.</p>
-            <button 
-              className="mt-4 text-blue-500 hover:underline"
-              onClick={() => window.location.href='/takeaways'}>
-              Read more tips →
-            </button>
-          </div>
-        </div>
-      </div>
+      <HomePage />
     </Layout>
   );
 }
